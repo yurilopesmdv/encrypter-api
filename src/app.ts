58d4210c1950949eaee4d5f8c294f632b188221e
Yuri Lopes Maria <yurilopesm@hotmail.com>
2023-10-21 19:00:40 -0300

    feat: router connected to main file

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import cors from "cors";
 import express, { json, Express } from "express";
 import { connectDb, disconnectDb } from "./config/database";
+import router from "./router";
 
 const app = express()
 
 app
   .use(cors())
   .use(json())
+  .use(router)
 
 export function init(): Promise<Express> {
   connectDb();
