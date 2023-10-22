72ee5d037d35e5757d8d8c79a633374493e9a690
Yuri Lopes Maria <yurilopesm@hotmail.com>
2023-10-22 17:57:27 -0300

    feat: sign-in and sign-up routes created

--- src/router/user.router.ts
+++ src/router/user.router.ts
@@ -1,5 +1,10 @@
+import userController from "@/controller/user.controller";
 import { Router } from "express";
 
 const userRouter = Router();
+
+userRouter
+  .post('/sign-up', userController.createUser)
+  .post('/sign-in', userController.login)
 
 export default userRouter;
