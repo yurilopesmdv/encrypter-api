757b470586ea921b00fcc3aa206f81501b452e44
Yuri Lopes Maria <yurilopesm@hotmail.com>
2023-10-22 17:57:55 -0300

    feat: create user and login created in controller

---
+++ src/controller/user.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+async function createUser(req: Request, res: Response) {
+
+}
+
+async function login(req: Request, res: Response) {
+
+}
+
+const userController = {
+  createUser,
+  login
+}
+
+export default userController;
